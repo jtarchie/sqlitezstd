c82729a58e20e815b464f7efaa66773dfb98623e
JT Archie <jtarchie@gmail.com>
2024-04-29 11:10:40 -0600

    add rtree benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -3,7 +3,6 @@ package sqlitezstd_test
 import (
 	"database/sql"
 	"fmt"
-	"io"
 	"math/rand"
 	"net/http"
 	"net/http/httptest"
@@ -12,12 +11,23 @@ import (
 	"path/filepath"
 	"runtime"
 	"testing"
+	"time"
 
 	sqlitezstd "github.com/jtarchie/sqlitezstd"
 	_ "github.com/mattn/go-sqlite3" // ensure you import the SQLite3 driver
 	"github.com/onsi/gomega/gexec"
 	"github.com/pioz/faker"
 )
+
+//nolint: gosec
+func randomBoundingBox() (float64, float64, float64, float64) {
+	minX := rand.Float64() * 100
+	maxX := minX + rand.Float64()*10
+	minY := rand.Float64() * 100
+	maxY := minY + rand.Float64()*10
+
+	return minX, maxX, minY, maxY
+}
 
 //nolint: gochecknoglobals
 var dbPath, zstPath string
@@ -51,6 +61,14 @@ func setupDB(b *testing.B) (string, string) {
 			value INTEGER,
 			sentence TEXT
 		);
+
+		CREATE VIRTUAL TABLE demo_rtree USING rtree(
+			id INTEGER PRIMARY KEY,
+			minX REAL,
+			maxX REAL,
+			minY REAL,
+			maxY REAL
+		);
 	`)
 	if err != nil {
 		b.Fatalf("Failed to create table: %v", err)
@@ -63,17 +81,32 @@ func setupDB(b *testing.B) (string, string) {
 
 	defer func() { _ = transaction.Rollback() }()
 
-	insert, err := transaction.Prepare("INSERT INTO entries (value, sentence) VALUES (?, ?)")
+	insertEntry, err := transaction.Prepare("INSERT INTO entries (value, sentence) VALUES (?, ?)")
 	if err != nil {
-		b.Fatalf("Failed to insert prepare: %v", err)
+		b.Fatalf("Failed to entry prepare: %v", err)
 	}
-	defer insert.Close()
+	defer insertEntry.Close()
 
-	for range 1_000_000 {
+	insertRtree, err := transaction.Prepare("INSERT INTO demo_rtree (id, minX, maxX, minY, maxY) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		b.Fatalf("Failed to rtree prepare: %v", err)
+	}
+	defer insertRtree.Close()
+
+	for index := range 1_000_000 {
 		//nolint: gosec
-		_, err = insert.Exec(rand.Int63(), faker.Sentence())
+		_, err = insertEntry.Exec(rand.Int63(), faker.Sentence())
 		if err != nil {
 			b.Fatalf("Failed to insert data: %v", err)
+		}
+
+		if index%10 == 0 {
+			minX, maxX, minY, maxY := randomBoundingBox()
+
+			_, err = insertRtree.Exec(index, minX, maxX, minY, maxY)
+			if err != nil {
+				b.Fatalf("Failed to insert data: %v", err)
+			}
 		}
 	}
 
@@ -107,12 +140,22 @@ func setupDB(b *testing.B) (string, string) {
 		// "-q", "22",
 	)
 
-	session, err := gexec.Start(command, io.Discard, io.Discard)
+	session, err := gexec.Start(command, os.Stderr, os.Stderr)
 	if err != nil {
 		b.Fatalf("Failed to compress data: %v", err)
 	}
 
-	session.Wait("10s")
+	timeout := time.NewTimer(10 * time.Second)
+	defer timeout.Stop()
+
+	select {
+	case <-session.Exited:
+		if session.ExitCode() != 0 {
+			panic("something went wrong compressing")
+		}
+	case <-timeout.C:
+		panic("something timeout wrong compressing")
+	}
 
 	return dbPath, zstPath
 }
@@ -135,6 +178,37 @@ func BenchmarkReadUncompressedSQLite(b *testing.B) {
 		var count int
 		for pb.Next() {
 			err = client.QueryRow("SELECT MAX(value) FROM entries").Scan(&count)
+			if err != nil {
+				b.Fatalf("Query failed: %v", err)
+			}
+		}
+	})
+}
+
+func BenchmarkReadUncompressedRtreeSQLite(b *testing.B) {
+	dbPath, _ := setupDB(b)
+
+	client, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		b.Fatalf("Failed to open database: %v", err)
+	}
+	defer client.Close()
+
+	client.SetMaxOpenConns(max(4, runtime.NumCPU()))
+
+	b.ResetTimer() // Start timing now.
+
+	b.RunParallel(func(pb *testing.PB) {
+		var count int
+		for pb.Next() {
+			err = client.QueryRow(`
+				SELECT
+					COUNT(*)
+				FROM demo_rtree
+				WHERE
+					minX <= -1 AND 1 <= maxX AND 
+					minY <= -1 AND 1 <= maxY 
+			`).Scan(&count)
 			if err != nil {
 				b.Fatalf("Query failed: %v", err)
 			}
@@ -285,6 +359,37 @@ func BenchmarkReadCompressedHTTPSQLite(b *testing.B) {
 		var count int
 		for pb.Next() {
 			err = client.QueryRow("SELECT MAX(value) FROM entries").Scan(&count)
+			if err != nil {
+				b.Fatalf("Query failed: %v", err)
+			}
+		}
+	})
+}
+
+func BenchmarkReadCompressedRtreeSQLite(b *testing.B) {
+	_, zstPath := setupDB(b)
+
+	client, err := sql.Open("sqlite3", fmt.Sprintf("%s?vfs=zstd", zstPath))
+	if err != nil {
+		b.Fatalf("Failed to open database: %v", err)
+	}
+	defer client.Close()
+
+	client.SetMaxOpenConns(max(4, runtime.NumCPU()))
+
+	b.ResetTimer() // Start timing now.
+
+	b.RunParallel(func(pb *testing.PB) {
+		var count int
+		for pb.Next() {
+			err = client.QueryRow(`
+				SELECT
+					COUNT(*)
+				FROM demo_rtree
+				WHERE
+					minX <= -1 AND 1 <= maxX AND 
+					minY <= -1 AND 1 <= maxY 
+			`).Scan(&count)
 			if err != nil {
 				b.Fatalf("Query failed: %v", err)
 			}
